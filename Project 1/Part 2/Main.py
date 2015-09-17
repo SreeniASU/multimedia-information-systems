972b3f69a8af0ea3d8dcbd5af5da8d5d9d8df546
James <perry.james.rm@gmail.com>
2015-09-16 18:33:01 -0700

    3/4 complete with Part 2
    Makes two grayscale frames, the difference image between them (delta),
    then normalizes delta

--- Project 1/Part 2/Main.py
+++ Project 1/Part 2/Main.py
@@ -1,90 +1,110 @@
-#James Perry - 1204873588
-#CSE408 Project 1 - Part 2
-#=====================================================================================================================
-#Description:
-#Implement a second program which lets the user select
-#- a video file, v, in the database,
-#- a frame rate, r,
-#- two frames, f1 and f2, and
-#- a color map file
-#given these,
-#- extract and displays 8-bit gray scale contents, g1 and g2, of frames, f1 and f2,
-#- compute a difference image, DELTA, of gray scale images, g1 and g2,
-#- rescales the values in the difference image to a range between -1.0 and 1.0 (such that -1.0 corresponds to
-# 255 and 1.0 corresponds to 255), and
-#- visualizes the difference image ? using the selected color map.
-#=====================================================================================================================
 import numpy as np
 import cv2
-import os
+import pickle
 
-#directory where files are stored
-rootDir = "Tests"
-fileSet = set()
-fileNameSet = set()
+#function to get the number of frames of a video
+def getNumberOfFrames(vid):
+	counter = 1
 
-#this extracts all files from our directory
-for dir_, _, files in os.walk(rootDir):
-    for fileName in files:
-        relDir = os.path.relpath(dir_, rootDir)     #get path to directory
-        relFile = os.path.join(relDir, fileName)    #get file in that directory
-        fileSet.add(relFile)
-        relFile = os.path.basename(relFile)         #remove path from name
-        fileNameSet.add(relFile)                        #add it to out list of files
+	while(vid.isOpened()):
+		ret,frame = vid.read()
 
-#collect our user input for processing
-print('======= List of Video Files =======')
-print '\n'.join(str(p) for p in fileNameSet)
-print('===================================')
-#print information to collect user inpput
-videoName = raw_input('Enter the name of the file you wish to read: ')
-#frameRateRead = raw_input("Enter desired frame rate: ")
-frame1Read = raw_input("Enter the first frame to compare: ")
-frame2Read = raw_input("Enter the second frame to compare: ")
-#colorMapFile = raw_input("Please select a color map file: ")
+		if ret:
+			counter += 1
+		else:
+			vid.release()
+			break
 
-#need colormap file and framerate support
+	return counter - 1
 
-#get the video by name from our file list
-selectedVideo = ""
-for file in fileSet:
-    if os.path.basename(file) == videoName:
-        selectedVideo = file
-        break
+video = cv2.VideoCapture('1.mp4')
 
-#open the video
-cap = cv2.VideoCapture(selectedVideo)
-#get the user entered frames from the video
-frame1 = ""
-frame2 = ""
+n_frames = getNumberOfFrames(video)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+video = cv2.VideoCapture('1.mp4') #we have to get the capture again after the 'getNumberOfFrames' function
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#initialization of 'frame1Read' and 'frame2Read' variables
+frame1Read = n_frames + 1
+frame2Read = n_frames + 1
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#while loop to validate user input
+while (frame1Read > n_frames or frame2Read > n_frames or frame1Read <= 0 or frame2Read <= 0):
+	print("Number of frames of the video: " + str(n_frames))
+	frame1Read = int(raw_input("Enter the first frame to compare: "))
+	frame2Read = int(raw_input("Enter the second frame to compare: "))
 
-cap.release()
+	if (frame1Read > n_frames or frame2Read > n_frames):
+		raw_input('Invalid frame number(s)!')
+
+count = 1
+
+gray1 = ""
+gray2 = ""
+
+while(video.isOpened()):
+	ret,frame = video.read()
+
+
+	if ret: #if video is still running...
+
+		if frame1Read == count:
+			#cv2.imshow('frame1Read',frame)
+			#cv2.waitKey(0)
+			gray1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			#cv2.waitKey(0)
+			#cv2.imwrite('frame' + str(frame1Read) + '.png',frame)
+		elif frame2Read == count:
+			gray2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			#cv2.imshow('frame2Read',frame)
+			#cv2.waitKey(0)
+			#cv2.imwrite('frame' + str(frame2Read) + '.png',frame)
+
+		count += 1
+	else:
+		break
+
+cv2.imshow('frame1',gray1)
+cv2.waitKey(0)
+
+cv2.imshow('frame2',gray2)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+gray_delta = gray1 - gray2
 
+cv2.imshow('delta',gray_delta)
+cv2.waitKey(0)
 
+#new_image = cv2.cvtColor(gray_delta,cv2.CV_32F)
+#cv2.imshow('delta',new_image)
+#cv2.waitKey(0)
 
+#gray1.convertTo(dst, CV_32F)
 
-displayGrayscale = raw_input("Display the grayscale representations of frames?(Y/N): ")
-if displayGrayscale == "Y":
-    #create our grayscale representations of these frames
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    #display these grayscale representations to the user
-    cv2.imshow('Grayscale of Frame 1',gray1)
-    cv2.imshow('Grayscale of Frame 2',gray2)
-    hideGrayscale = raw_input("Press any key to continue and close grayscale images: ")
-    if hideGrayscale != "":
-      cv2.destroyAllWindows()
-else:
-    displayGrayscale = raw_input("Display the DELTA difference images?(Y/N): ")
+high = 1.0
+low = -1.0
 
+mins = np.min(gray_delta, axis=0)
+maxs = np.max(gray_delta, axis=0)
+rng = maxs - mins
+
+scaled_points = high - (((high - low) * (maxs - gray_delta)) / rng)
+
+#text_file = open('new_grayscale.txt','w')
+
+#text_file.write(gray_delta)
+
+#text_file.write('\n\n\n %s' % scaled_points)
+
+f = open("new_grayscale.txt", "w")
+f.write("".join(map(lambda x: str(x), scaled_points)))
+f.close()
+
+#print("gray delta new: ",scaled_points)
+cv2.imshow('gray delta new',scaled_points)
+cv2.waitKey(0)
+
+print(count)
+
+video.release()
+cv2.destroyAllWindows()
